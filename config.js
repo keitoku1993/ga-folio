c6a41a8dec8a49cf1f1a782c2c7459bf76c0a5a6
keitoku1993 <keitoku1993@gmail.com>
2019-09-10 01:17:56 +0900

    pathprefix

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-prologue/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/ga-folio`, // This path is subpath of your hosting https://domain/portfolio
   authorName: '@keitoku1993',
   heading: 'Web Frontend Developer',
   // social
